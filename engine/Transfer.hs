a1dec929a4b641d8efb68b1b9aa56e30f069cbec
Dr Bean <drbean@localhost.localdomain>
2016-06-13 17:58:26 +0800

    ported:drbean/Transfer.hs

--- engine/Transfer.hs
+++ engine/Transfer.hs
@@ -1,6 +1,6 @@
 module Main where
 
-import Chat
+import Communication
 import LogicalForm hiding ((==))
 import Evaluation
 
@@ -20,57 +20,26 @@ import System.Environment.FindBin
 main :: IO ()
 main = do
 	path <- getProgPath
-	gr <- readPGF ( path ++ "/Chat.pgf" )
+	gr <- readPGF ( path ++ "/Communication.pgf" )
+	let lang = languages gr
+	let morpho = buildMorpho gr $ head lang
 	hClose stderr
 	hDuplicateTo stdout stderr
 	s <- getLine
 	let l = (chomp . lc_first) s
-	putStrLn ("Unknown_words: " ++ (unknown l) )
+	let unknown = unwords (morphoMissing morpho (words l))
+	putStrLn ("Unknown_words: " ++ unknown )
 	let ps = parses gr l
-	let ls = map ((linear gr) <=< transform) ps
-	putStrLn ("Parsed: " ++ (show (map (showExpr []) ps) ) )
+	let ls = map (linear gr <=< transform) ps
+	putStrLn ("Parsed: " ++ show (map (showExpr []) ps ) )
 	let urs = map (unmaybe . rep) ps
 	-- let reps = map (\ur -> ur (term2ref drsRefs var_e)) urs
 	-- putStrLn ("Representation: " ++ show reps )
 	-- let lfs = map (\ur -> drsToLF (ur (term2ref drsRefs var_e))) urs
 	-- putStrLn ("LF: " ++ show lfs )
-	putStrLn ("Answer: " ++ (bestAnswer ls) )
+	putStrLn ("Answer: No answer" )
 	let courses = map (label . fg) ps
 	putStrLn ("Course: " ++ foldl takeCourse "Unparseable" courses )
-
-unknown :: String -> String
-unknown ws = intercalate ", " ( filter (\x -> not (checkLists x ws) ) (words ws))
-
-checkLists :: String -> String -> Bool
-checkLists w ws	=	if check_on_wordlist [w] then True
-									else if check_on_wordlist (alternatives w (bigram ws)) then True
-									else if check_on_wordlist (alternatives w (trigram ws)) then True
-									else False
-splitVariants :: [String] -> [String]
-splitVariants ls = concat $ map (splitOn ", ") ls
-
-check_on_wordlist :: [String] -> Bool
-check_on_wordlist = any (flip elem (splitVariants wordlist))
-
-alternatives :: String -> [(String, String)] -> [String]
-alternatives w bis	= [ bi | (key,bi) <- bis, key==w]
-
-bigram :: String -> [ (String, String) ]
-bigram ws = let zs = zip ss sss
-						where
-						ss = words ws
-						sss = tail ss
-						in (map (\(a,b) -> (a, unwords [a,b]) ) zs) ++
-							(map (\(a,b) -> (b, unwords [a,b]) ) zs)
-
-trigram :: String -> [ (String, String) ]
-trigram ws = let zs = zip3 ss sss ssss
-			where
-			ss = words ws
-			sss = tail ss
-			ssss = tail sss
-			in (map (\(a,b,c) -> (a, unwords [a,b,c]) ) zs) ++
-				(map (\(a,b,c) -> (c, unwords [a,b,c]) ) zs)
 
 label :: GUtt -> String
 label (GQUt (GPosQ (GWH_Pred _ _)))	= "WH"
@@ -121,11 +90,11 @@ collateAnswer a b = formatUp $ nub $ filter
 	|| x ==	"Alice"
 	|| x ==	"Ariel"
 	|| x ==	"Sabrina"
-	) (concat $ map (splitOn " , " ) (splitOn " or " (a ++ " , " ++ b)))
+	) (concatMap (splitOn " , " ) (splitOn " or " (a ++ " , " ++ b)))
 
 formatUp es = let parts = splitAt 1 (reverse es)
-	in case (snd parts) of 
+	in case snd parts of 
 		[] -> concat (fst parts)
-		_ -> concat  ((intersperse " , " (snd parts)) ++ [" or "] ++ (fst parts) )
+		_ -> concat  (intersperse " , " (snd parts) ++ [" or "] ++ fst parts )
 --
 -- vim: set ts=2 sts=2 sw=2 noet:
